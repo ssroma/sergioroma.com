b6a065c4676ddc857d0f8c3beda2395e3da44ca0
Sergio Roma <roma.sergio@gmail.com>
2019-06-10 04:28:55 +0100

    Site Half way done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LandingComponent } from './landing/landing.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LandingComponent,
+    AboutMeComponent,
+    ProjectComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // Unable fragments 
+    RouterModule.forRoot([], {
+      anchorScrolling: 'enabled',
+      onSameUrlNavigation: 'reload',
+      scrollPositionRestoration: 'enabled'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations'
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.sass'],
+  animations: [
+    trigger('links', [
+      state('closed', style({
+        left: '999px'
+      })),
+      state('openned', style({
+        left: '0px'
+      })),
+      transition('closed <=> openned', animate(1000))
+    ])
+  ]
+})
+export class NavComponent implements OnInit {
+
+  showHideNav = 'closed';
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+  onOpen(links: HTMLElement){
+    console.log('From close button.' + links)
+    this.showHideNav == 'closed' ? this.showHideNav = 'openned' : this.showHideNav = 'closed'
+    //links.style.left = '0px'
+  }
+
+  onClose(links: HTMLElement){
+    console.log('From close button.' + links)
+    this.showHideNav == 'openned' ? this.showHideNav = 'closed' : this.showHideNav = 'openned'
+    //links.style.left = '900px'
+  }
+
+  closeNav(){
+    console.log(this.showHideNav)
+    if(this.showHideNav == 'openned'){
+      this.showHideNav == 'openned' ? this.showHideNav = 'closed' : this.showHideNav = 'openned'
+    }
+  }
+}
